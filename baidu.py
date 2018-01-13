7b90bab7cc6192472211abbf070075cfe0d6df79
Shuming Fang <fangshuming519@gmail.com>
2018-01-13 16:52:52 +0800

    Update baidu.py

--- baidu.py
+++ baidu.py
@@ -1,5 +1,10 @@
 #爬取百度一下
-url = "http://www.baidu.com/s?wd=" + '%E5%A4%AA%E8%B4%B5%E5%95%A6&oq'
+# -*- coding:utf8 -*-
+from urllib import request
+from urllib.parse import urlencode
+from urllib.request import urlopen
+data = urlencode({'wd':'你'})
+url = "http://www.baidu.com/s?" + data
 resp = urlopen(url)
 content = resp.read()
 content = content.decode('utf-8')
