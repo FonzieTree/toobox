35664fd34f952327ff3db5b0290843b2d0998963
Shuming Fang <fangshuming519@gmail.com>
2018-01-13 16:56:02 +0800

    Update baidu.py

--- baidu.py
+++ baidu.py
@@ -10,3 +10,19 @@ content = resp.read()
 content = content.decode('utf-8')
 soup = BeautifulSoup(content,"lxml")
 text = soup.get_text().split()
+
+
+#爬取百度字典
+#! /usr/bin/python
+# -*- coding:utf-8 -*- 
+import requests
+from bs4 import BeautifulSoup
+import time
+import random
+result = []
+html = requests.post('http://hanyu.baidu.com/zici/s?wd='+'百'+'&query='+'百'+'&srcid=28232&from=kg0&from=kg0')
+html.encoding = html.apparent_encoding 
+html = html.text
+soup = BeautifulSoup(html,"lxml")
+text = soup.find_all('div', class_='tab-content')
+BeautifulSoup(content,"lxml")
