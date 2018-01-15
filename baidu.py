3fcba70b64ab5ca579eb0f15f584d3f2b242efc0
Shuming Fang <fangshuming519@gmail.com>
2018-01-15 17:50:42 +0800

    Update baidu.py

--- baidu.py
+++ baidu.py
@@ -15,19 +15,22 @@ text = soup.get_text().split()
 #爬取百度字典
 #! /usr/bin/python
 # -*- coding:utf-8 -*- 
+#! /usr/bin/python
+# -*- coding:utf-8 -*- 
 import requests
 from bs4 import BeautifulSoup
 import re
-result = []
 def baidu(data):
     html = requests.post('http://hanyu.baidu.com/zici/s?wd='+data+'&query='+data+'&srcid=28232&from=kg0&from=kg0')
     html.encoding = html.apparent_encoding 
     html = html.text
     soup = BeautifulSoup(html,"lxml")
-    return(data+'，'+soup.p.contents[0][2:]+'。')
+    result = soup.p.contents[0]
+    result, num = re.subn(string = result,pattern = '1.',repl = '')
+    return(data + '，' + result)
 i = 0
 with open(r'../input.txt','r',encoding='utf-8') as file:
-    with open('../baiducidian.txt','w', encoding='utf-8') as baiducidian:
+    with open('../output.txt','w', encoding='utf-8') as baiducidian:
         chars = file.readlines()
         for char in chars:
             data = char.strip()
