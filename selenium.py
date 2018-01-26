b232531f3ed58d492654a7ef43319f367ed20ef4
Fonzie <675688853@qq.com>
2018-01-26 11:50:17 +0800

    Update selenium.py

--- selenium.py
+++ selenium.py
@@ -5,7 +5,10 @@ from selenium.webdriver.common.keys import Keys
 browser = webdriver.Firefox('D:/Program Files/Mozilla Firefox')
 browser.get('http://ai.baidu.com/tech/nlp/lexical')  
 inputElement = browser.find_elements_by_class_name("com-txt")[0]
-inputElement.send_keys('慕尼黑再保险公司不仅是此类行动的倡议者，更是将其大量气候数据整合进保险产品中，并与公众共享大量天气信息，参与到新能源领域的保障中。')
+inputElement.clear()
+inputElement.send_keys('中文分词项目(开源/API接口)总结 - CSDN博客')
 inputElement.send_keys(Keys.ENTER)
-outputElement = browser.find_elements_by_class_name("result-left")[0]
-text = outputElement.text
+outputElement = browser.find_elements_by_class_name("result-left")
+text = outputElement[0].text
+text = re.findall(r'\w+\n', text)
+print(text)
