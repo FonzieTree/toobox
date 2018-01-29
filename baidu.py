ae5b7017bb6d8874a4945731925ba517431f0c03
Fonzie <675688853@qq.com>
2018-01-29 11:59:30 +0800

    Update baidu.py

--- baidu.py
+++ baidu.py
@@ -40,3 +40,38 @@ with open(r'../input.txt','r',encoding='utf-8') as file:
             baiducidian.flush()
             i += 1
             print(i)
+
+#百度依存句法分词
+# -*- coding: utf-8 -*-                  
+import os
+import time
+import random                   
+from selenium import webdriver              
+from selenium.webdriver.common.keys import Keys 
+os.chdir(r'./')
+profile.update_preferences()
+browser = webdriver.Firefox('./Mozilla Firefox')
+browser.get('http://ai.baidu.com/tech/nlp/lexical')  
+inputElement = browser.find_elements_by_class_name("com-txt")[0]
+i = 0
+with open('result.txt','w') as file1:
+    with open('data.txt','r',encoding='utf-8') as file2:
+        data = file2.readlines()
+        for line in data:
+            time.sleep(random.uniform(1,5))
+            line = ''.join(line[17:].strip().split())
+            inputElement.clear()
+            inputElement.send_keys(line)
+            inputElement.send_keys(Keys.ENTER)
+            outputElement = browser.find_elements_by_class_name("result-tips")
+            output = ['/'.join(e.text.split('\n')) for e in outputElement]
+            output = ' '.join(output)
+            file1.write(output)
+            file1.write('\n')
+            file1.flush()
+            i += 1
+            if i % 500 == 0:
+                time.sleep(3600)	
+            print(output)
+            
+print('Done')
