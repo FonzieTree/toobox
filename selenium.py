395fb1eec041f04118674211a5c28949d2fdaaae
Fonzie <675688853@qq.com>
2018-01-31 15:42:51 +0800

    Update selenium.py

--- selenium.py
+++ selenium.py
@@ -4,20 +4,15 @@ import time
 import random                   
 from selenium import webdriver              
 from selenium.webdriver.common.keys import Keys 
-os.chdir(r'D:\')
+os.chdir(r'...')
 browser = webdriver.Firefox('D:/Program Files/Mozilla Firefox')
 browser.get('http://ai.baidu.com/tech/nlp/lexical')
-i = 19959
+i = 0
 with open('result.txt','a') as file1:
     with open('data.txt','r',encoding='utf-8') as file2:
         data = file2.readlines()
         data = data[i:]
         for line in data:
-            if i % 10 == 0:
-                browser.get('https://www.sogou.com/')
-                time.sleep(0.1)
-                browser.get('http://ai.baidu.com/tech/nlp/lexical')
-                time.sleep(5)
             try:
                 inputElement = browser.find_elements_by_class_name("com-txt")[0]
                 line = ''.join(line[17:].strip().split())
@@ -28,6 +23,7 @@ with open('result.txt','a') as file1:
                 outputElement = browser.find_elements_by_class_name("result-tips")
                 output = ['/'.join(e.text.split('\n')) for e in outputElement]
                 output = ' '.join(output)
+                print(i, output)
                 file1.write(output)
                 file1.write('\n')
                 file1.flush()
@@ -35,18 +31,15 @@ with open('result.txt','a') as file1:
                 if output == '':
                     print('Null value')
                     browser.get('https://www.sogou.com/')
-                    time.sleep(0.1)
-                    browser.close()
+                    browser.quit()
+                    time.sleep(5)
                     browser = webdriver.Firefox('D:/Program Files/Mozilla Firefox')
                     browser.get('http://ai.baidu.com/tech/nlp/lexical')
-                    time.sleep(5)
-                print(i, output)
             except:
                 print('Exception')
                 browser.get('https://www.sogou.com/')
-                time.sleep(0.1)
-                browser.close()
+                browser.quit()
+                time.sleep(5)
                 browser = webdriver.Firefox('D:/Program Files/Mozilla Firefox')
                 browser.get('http://ai.baidu.com/tech/nlp/lexical')
-                time.sleep(5)
 print('Done')
