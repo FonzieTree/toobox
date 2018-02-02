ecd16ae0cc0a2261ca1829d6bcdfa727f9385252
Fonzie <675688853@qq.com>
2018-02-02 15:49:16 +0800

    Update selenium.py

--- selenium.py
+++ selenium.py
@@ -7,6 +7,7 @@ from selenium.webdriver.common.keys import Keys
 os.chdir(r'...')
 browser = webdriver.Firefox('D:/Program Files/Mozilla Firefox')
 browser.get('http://ai.baidu.com/tech/nlp/lexical')
+i = 36549
 with open('result.txt','a') as file1:
     with open('data.txt','r',encoding='utf-8') as file2:
         data = file2.readlines()
@@ -15,31 +16,33 @@ with open('result.txt','a') as file1:
             try:
                 inputElement = browser.find_elements_by_class_name("com-txt")[0]
                 line = ''.join(line[17:].strip().split())
-                inputElement.clear()
-                inputElement.send_keys(line)
-                inputElement.send_keys(Keys.ENTER)
-                time.sleep(3)
-                outputElement = browser.find_elements_by_class_name("result-tips")
-                output = ['/'.join(e.text.split('\n')) for e in outputElement]
-                output = ' '.join(output)
-                print(i, output)
-                file1.write(output)
-                file1.write('\n')
-                file1.flush()
-                i += 1
-                if output == '':
-                    print('Null value')
-                    browser.quit()
-                    browser = webdriver.Firefox('D:/Program Files/Mozilla Firefox')
-                    browser.get('https://www.sogou.com/')
-                    time.sleep(5)
-                    browser.get('http://ai.baidu.com/tech/nlp/lexical')
+                if line!='':
+                    inputElement.clear()
+                    inputElement.send_keys(line)
+                    inputElement.send_keys(Keys.ENTER)
+                    time.sleep(1)
+                    outputElement = browser.find_elements_by_class_name("result-tips")
+                    output = ['/'.join(e.text.split('\n')) for e in outputElement]
+                    output = ' '.join(output)
+                    if output !='':
+                        print(i, output)
+                        file1.write(output)
+                        file1.write('\n')
+                        file1.flush()
+                        i += 1
+                    elif output =='':
+                        print('Null value')
+                        browser.quit()
+                        browser = webdriver.Firefox('D:/Program Files/Mozilla Firefox')
+                        browser.get('https://www.sogou.com/')
+                        browser.get('http://ai.baidu.com/tech/nlp/lexical')
+                        time.sleep(5)
             except:
                 print('Exception')
                 browser.get('https://www.sogou.com/')
                 browser.quit()
-                time.sleep(5)
                 browser = webdriver.Firefox('D:/Program Files/Mozilla Firefox')
                 browser.get('https://www.sogou.com/')
                 browser.get('http://ai.baidu.com/tech/nlp/lexical')
+                time.sleep(5)
 print('Done')
